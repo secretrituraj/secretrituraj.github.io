e450746b8a81729402deda29024e046ba7cb13e9
secretrituraj <rituraj.sharma@st-hildas.ox.ac.uk>
2025-01-21 16:01:45 +0000

    aesthetic changes

--- games/wordle/script.js
+++ games/wordle/script.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const keys = [
       ['q','w','e','r','t','y','u','i','o','p'],
       ['a','s','d','f','g','h','j','k','l'],
-      ['enter','z','x','c','v','b','n','m','backspace']
+      ['backspace','z','x','c','v','b','n','m','enter']
     ];
 
     keys.forEach(row => {
